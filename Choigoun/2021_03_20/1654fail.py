773175976606fc85335690e7d411dee7f5d55907
gwcat0506 <gwcat0506@naver.com>
2021-03-20 19:36:18 +0900

    20210320 update

---
+++ Choigoun/2021_03_20/1654fail.py
@@ -0,0 +1,8 @@
+﻿import sys
+
+n,c = map(int,sys.stdin.readline().split()) # input
+
+line_list = []
+for _ in range(n):
+    x = int(input())
+    line_list.append(x)
---
+++ Choigoun/2021_03_20/2805fail.py
@@ -0,0 +1,22 @@
+﻿
+import sys
+#시간초과 
+N, M = map(int,sys.stdin.readline().split()) # N 과 M에 각각 숫자 input
+tree = list(map(int, input().split()))
+start = 1
+end = max(tree) 
+
+while start <= end:
+    mid = (start + end)//2
+
+    tree_cut = 0 #자른 나무들의 합
+    for i in tree:
+        if i >=mid:
+            tree_cut += i-mid
+    
+    if tree_cut >= M:
+        start = mid + 1
+    else:
+        end = mid -1
+
+print(end)
